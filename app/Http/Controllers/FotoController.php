f4e19b6d4083c832016ff6b92f2c3998f5368764
Rodrigo Funes Biasatti <rodrigo11.funes@hotmail.com>
2019-12-19 01:46:37 -0300

    modified fotoscontroller

--- app/Http/Controllers/FotoController.php
+++ app/Http/Controllers/FotoController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class FotoController extends Controller
 {
     public function create(Request $request){
-        dd($request);
+        //dd($request);
+        dd($request->foto);
         
     }
 }
