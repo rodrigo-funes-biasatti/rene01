d9f4cb1d1d0e5bc08d9dba27454f343fda589cbb
Máximo funes <rodrigo11.funes@hotmail.com>
2019-12-21 23:08:36 -0300

    Fixed some things from FotoController

--- app/Http/Controllers/FotoController.php
+++ app/Http/Controllers/FotoController.php
@@ -4,15 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Foto;
+use File;
 
 class FotoController extends Controller
 {
     public function create(Request $request)
     {
+        $uploadFile = $request->file('foto');
         //dd($request);
         //dd($request->foto);
+        //dd($uploadFile);
         $foto = new Foto;
-        $imageName = time() . "." . $request->foto->extension();
+        $imageName = $uploadFile->getClientOriginalName();
         $foto->filename = $imageName;
         $foto->descripcion = $request->input('descripcion');
         $request->foto->move(public_path('img/Galeria'), $imageName);
@@ -27,10 +30,15 @@ class FotoController extends Controller
     public function delete($id)
     {
         //dd($id);
-        return redirect()->route('home')->with('fail-delete-foto', 'No se ha podido eliminar la foto con éxito.');
+        //return redirect()->route('home')->with('fail-delete-foto', 'No se ha podido eliminar la foto con éxito.');
         if(Foto::where('foto_id', "=", $id)->exists())
         {
             $foto = Foto::where('foto_id', "=", $id);
+            //dd($foto);
+            $foto_path = public_path('img\Galeria\\' . $foto->first()->filename);
+            //dd($foto_path);
+
+            File::delete($foto_path);
             $foto->delete();
             return redirect()->route("home")->with('delete-foto', 'Se ha eliminado la fotode la galería con éxito.');
         }
